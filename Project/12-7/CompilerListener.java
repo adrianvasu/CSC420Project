07de252fc77cc602a792891eaedc288b7c3698e3
Adrian <32743531+adrianvasu@users.noreply.github.com>
2017-12-07 03:02:59 -0500

    Final Commit
    Finished compiler that translates baisc MountC files into Pep/9 assembly language.

--- Project/12-7/CompilerListener.java
+++ Project/12-7/CompilerListener.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class CompilerListener extends MountCBaseListener {
 
     private Map<String, Integer> symTab = new HashMap<String, Integer>();
+    private Map<String, Integer> parameters = new HashMap<String, Integer>();
     int ifIdGenerator = 1;
     int addCounter = 0;
     int ifCounter = 0;
@@ -10,13 +11,11 @@ public class CompilerListener extends MountCBaseListener {
     boolean onlyIfBool = false;
     boolean ifEndBool = false;
     int howManyIf = 0;
-    String currentId;
-    int numChildren = 1;
-    int stackLoc = -4;
-    int numberParameters;
-    ArrayList<String> funNames = new ArrayList<>();
-
-    int location = 0;
+    String currentID = "";
+    ArrayList<String> paramNames = new ArrayList<>();
+    boolean inFunCall = false;
+    int params = 0;
+    int totalNumParams = 0;
 
     @Override
     public void enterProgram(MountCParser.ProgramContext ctx) {
@@ -24,13 +23,11 @@ public class CompilerListener extends MountCBaseListener {
         System.out.println("\tCALL\tmain");
         System.out.println("\tADDSP\t2,i");
         System.out.println("\tSTOP");
-
         System.out.println("putint:\tDECO\t2,s");
         System.out.println("\tLDWA\t2,s");
         System.out.println("\tSTWA\t4,s");  //  return the same value we printed out.
         System.out.println("\tRET");
         symTab.put("putint", 1);
-
         System.out.println("getint:\tDECI\t2,s");
         System.out.println("\tRET");
         symTab.put("getint", 0);
@@ -46,67 +43,61 @@ public class CompilerListener extends MountCBaseListener {
         System.out.println("\tSTWA\t2,s");
         System.out.println("\tRET");
         symTab.put("getchar", 0);
-
     }
 
     @Override
     public void exitProgram(MountCParser.ProgramContext ctx) {
         System.out.println("\t.END");
     }
-/*
-    public int numParams(MountCParser.Fun_defContext ctx, int x) {
-        try {
-            MountCParser.ParameterContext id = ctx.getChild(2).getChild(1).getChild(2);
-            for (int i = 0; i < x; i += 1) {
-                id.
-            }
-        } catch (Exception ex) {
-            String id = "";
-        }
-        while (!id.isEmpty()) {
-            answer += numParams(ctx, x + 1);
-        }
-        return answer;
-    }
-*/
 
     @Override
     public void enterFun_def(MountCParser.Fun_defContext ctx) {
-        currentId = ctx.ID().toString();
         int numKids = ctx.getChild(2).getChildCount();
-        boolean mainCheck = ctx.ID().toString().equals("main");
+        int numParams = 0;
+        if (numKids != 0) {
+            numParams = (((numKids) / 2) + 1);
+        } else {
+            numParams = 0;
+        }
+
+        String id = ctx.ID().toString();
+
+        if (symTab.containsKey(id)) {
+            System.err.println("error function is defined");
+            System.exit(4);
+        } else {
+            symTab.put(ctx.ID().toString(), numParams);
+        }
+
+        int numBytesToAdd = 2 * (symTab.get(id));
+
+        //System.out.println(numBytesToAdd);
+
+        for (int i = numKids - 1; i >= 0; i -= 2) {
+            //System.out.println(i);
+            parameters.put(id + "_" + ctx.getChild(2).getChild(i).toString(), numBytesToAdd);
+            paramNames.add(id + "_" + ctx.getChild(2).getChild(i).toString());
+            //System.out.println(id + "_" + ctx.getChild(2).getChild(i).toString() + " " + numBytesToAdd);
+            numBytesToAdd -= 2;
+            totalNumParams += 1;
+        }
+
         System.out.println(ctx.ID().toString() + ":\tNOP0");
-        if (!mainCheck) {
-            System.out.println("\tSUBSP\t2,i");
-        }
-        if (mainCheck) {
-            symTab.put(ctx.ID().toString(), 0);
-            funNames.add(ctx.ID().toString());
-        }
-        if (!mainCheck) {
-            symTab.put(ctx.ID().toString(), numKids);
-            funNames.add(ctx.ID().toString());
-        }
+
+        currentID = id;
+
     }
 
     @Override public void exitFun_def(MountCParser.Fun_defContext ctx) {
-        if (ctx.ID().toString().equals("main")) {
-            System.out.println("\tSTWA\t2,s");
-        }
         if ((!ctx.ID().toString().equals("main")) && (onlyIfBool == false) && (ifEndBool == false)) {
-            System.out.println("\tSTWA\t2,s");
             System.out.println("\tADDSP\t2,i");
         }
         System.out.println("\tRET");
-        numChildren = 1;
     }
 
     @Override
     public void enterId_list(MountCParser.Id_listContext ctx) {
-        //System.out.println(ctx.ID().toString() + ":\t.EQUATE\t0");
-        /*if (!ctx.getParent().getChild(0).getText().toString().equals("main")) {
-            paramList.add(ctx.getChild(0).getText().toString());
-        }*/
+
     }
 
     @Override
@@ -116,50 +107,21 @@ public class CompilerListener extends MountCBaseListener {
 
     @Override
     public void enterParameter(MountCParser.ParameterContext ctx) {
-        int check = ctx.getChild(2).getChildCount();
-        String param = ctx.ID().toString();
-        //System.out.println("param is" + param);
-        //System.out.println("symtab is" + symTab.get(currentId));
-        //System.out.println(check);
-        //System.out.println(param);
-        //System.out.println(ctx.getParent().getChild(0).getText().toString());
-        if (symTab.get(ctx.getParent().getChild(0).getText().toString()) == null) {
-            symTab.put(ctx.getParent().getChild(0).getText().toString(), location);
-            location += 2;
-            //System.out.println(location);
-            //paramList.add(ctx.getChild(1).getText().toString());
-        }
-        if (symTab.get(ctx.ID().toString()) == null) {
-            symTab.put(ctx.ID().toString(), location);
-            location += 2;
-            //paramList.add(ctx.getChild(1).getText().toString());
-        }
-
-        //System.out.println("father param is " + symTab.get(ctx.getParent().getChild(0).getText().toString()));
-        //System.out.println("son param is " + symTab.get(ctx.ID().toString()));
-        /*for (int i = 0; i < paramList.size(); i ++) {
-            System.out.println(paramList.get(i));
-        }*/
-        //symTab.put();
-    }
-
-    @Override
-    public void exitParameter(MountCParser.ParameterContext ctx) {
 
     }
 
     @Override
     public void enterNumTerm(MountCParser.NumTermContext ctx) {
         String id = ctx.getParent().getParent().getChild(0).getText().toString();
+
         if (id.equals("+") || id.equals("-")) {
             /* Do Nothing */
-        }
-        else if(ctx.getParent().getParent().getParent().getClass().toString().contains("FunCallContext")) {
-                int j = location;
-                System.out.println("\tLDWA\t'" + ctx.NUM().toString() + "',i");
-                System.out.println("\tSTWA\t" + (location - j) + ",s");
         } else {
             System.out.println("\tLDWA\t" + ctx.NUM() + ",i");
+            if (inFunCall) {
+                System.out.println("\tSTWA\t-" + parameters.get(paramNames.get(params)) + ",s");
+                params += 1;
+            }
         }
     }
 
@@ -169,24 +131,13 @@ public class CompilerListener extends MountCBaseListener {
         String id = ctx.getChild(1).getChild(0).getText().toString();
         String subCheck = ctx.getChild(0).getChild(0).getText().toString();
         String curNum = ctx.getChild(1).getChild(0).getText().toString();
-        boolean useParam = false;
         //System.out.println(id);
-
-        //System.out.println(curNum);
-        //System.out.println(symTab.get(curNum));
-        if (symTab.get(curNum) != null) {
-            useParam = true;
-        }
-        //System.out.println(useParam);
         if (id.equals("(")) {
             //System.out.println("\tSTWA\t-2,s");
         }
         else if (subCheck.equals("+")){
             if (curNum.contains("getint")) {
                 System.out.println("\tSTWA\t-6,s");
-            }
-            else if (useParam) {
-                System.out.println("\tADDA\t" + symTab.get(curNum) + ",s");
             } else {
                 System.out.println("\tADDA\t" + curNum + ",i");
             }
@@ -199,23 +150,11 @@ public class CompilerListener extends MountCBaseListener {
                 if(subCheck.equals("+")){
                     System.out.println("\tADDA\t0,s");
                 }
-                else if (useParam) {
-                    System.out.println("\tADDA\t" + symTab.get(curNum) + ",s");
-                }
-
-                else if (useParam) {
-                    System.out.println("\tNEGA\t");
-                    System.out.println("\tADDA\t-6,s");
-                    System.out.println("\tSUBA\t" + symTab.get(curNum) + ",s");
-                }
                 else if (subCheck.equals("-")) {
                     System.out.println("\tNEGA\t");
                     System.out.println("\tADDA\t-6,s");
                     System.out.println("\tSUBA\t" + curNum + ",i");
                 }
-            }
-            else if (useParam) {
-                System.out.println("\tSUBA\t" + symTab.get(curNum) + ",s");
             } else {
                 System.out.println("\tSUBA\t" + curNum + ",i");
             }
@@ -250,36 +189,24 @@ public class CompilerListener extends MountCBaseListener {
     }
 
     @Override
+    public void exitIdTerm(MountCParser.IdTermContext ctx) {
+        //System.out.println("\tCALL\t" + ctx.ID().toString());
+    }
+
+    @Override
     public void enterFunCall(MountCParser.FunCallContext ctx) {
         String id = ctx.getParent().getChild(0).toString();
         Integer numParams = symTab.get(id);
-        int checkParams = ctx.getChild(1).getChildCount();
-        if (checkParams == 1) {
-            //Do Nothing
-        }
-        else if ((checkParams % 2) == 0) {
-            System.err.println("error with parameters");
-            System.exit(2);
-        }
-        else if (checkParams == 3) {
-            checkParams -= 1;
-        } else {
-            checkParams -= 2;
-        }
-
-        //System.out.println(numParams);
-
-        //System.out.println(checkParams);
         if (numParams == null) {
            System.err.println("function " + id + " not defined at this point");
            System.exit(1);
         }
-        if (numParams != checkParams) {
+        if (numParams != (((ctx.getChild(1).getChildCount())/2) + 1)) {
            System.err.println("function " + id + " called with wrong number of arguments " + ctx.getChild(1).getChildCount());
            System.exit(2);
         }
-
-
+        inFunCall = true;
+        params = 0;
     }
 
     @Override
@@ -298,6 +225,12 @@ public class CompilerListener extends MountCBaseListener {
             int numBytesToPop = 2*(symTab.get(id) + 1);  //  Won't get here if id not in symtab.
             System.out.println("\tADDSP\t" + numBytesToPop + ",i");
         }
+        else if (id.equals("putchar")) {
+            System.out.println("\tSTWA\t0,s");
+            System.out.println("\tCALL\t" + id);
+            int numBytesToPop = 2*(symTab.get(id) + 1);  //  Won't get here if id not in symtab.
+            System.out.println("\tADDSP\t" + numBytesToPop + ",i");
+        }
         else if (id.equals("getchar")) {
             System.out.println("\tCALL\t" + id);
             int numBytesToPop = 2*(symTab.get(id) + 1);  //  Won't get here if id not in symtab.
@@ -305,19 +238,14 @@ public class CompilerListener extends MountCBaseListener {
             System.out.println("\tLDWA\t-2,s");
             System.out.println("\tSTWA\t0,s");
         }
-        else if (id.equals("putchar")) {
-            System.out.println("\tSTWA\t0,s");
-            System.out.println("\tCALL\t" + id);
-            int numBytesToPop = 2*(symTab.get(id) + 1);  //  Won't get here if id not in symtab.
-            System.out.println("\tADDSP\t" + numBytesToPop + ",i");
-        }
         else {
+            System.out.println("\tSUBSP\t" + parameters.get(paramNames.get(totalNumParams - params)) + ",i");
             System.out.println("\tCALL\t" + id);
             int numBytesToPop = 2*(symTab.get(id) + 1);  //  Won't get here if id not in symtab.
             System.out.println("\tADDSP\t" + numBytesToPop + ",i");
         }
-        //System.out.println("\tADDSP\t" + 2*numberParameters + ",i");
-        stackLoc = -4;
+        inFunCall = false;
+        params = 0;
     }
 
     @Override
@@ -410,55 +338,16 @@ public class CompilerListener extends MountCBaseListener {
         String id = ctx.getParent().getParent().getParent().getParent().getParent().getChild(0).getText().toString();
         String compare = "putchar";
         boolean loadId = id.regionMatches(true, 0, compare, 0, compare.length());
-
+        //System.out.println(ctx.getChild(0).toString());
+        if (parameters.containsKey(currentID + "_" + ctx.getChild(0).toString())) {
+            int location = parameters.get(currentID + "_" + ctx.getChild(0).toString());
+            System.out.println("\tLDWA\t" + location + ",s");
+        }
         if (loadId) {
             System.out.println("\tLDWA\t'" + ctx.ID() + "',i");
         }
 
-        if (symTab.get(ctx.ID().toString()) == null) {
-            symTab.put(ctx.ID().toString(), 0);
-        }
-        //System.out.println(symTab.get(ctx.ID().toString()));
-        boolean funNameCheck = false;
-        for (int j = 0; j < funNames.size(); j ++) {
-            if (ctx.ID().toString().contains(funNames.get(j))) {
-                funNameCheck = true;
-            }
-        }
-
-        //System.out.println("HI");
-        //System.out.println(ctx.getParent().getParent().getParent().getParent().getChild(0).getText().toString().contains("main"));
-        //System.out.println(funNameCheck);
-        if ((funNameCheck == false)) {
-            System.out.println("\tLDWA\t" + ctx.ID().toString() + ",i");
-            System.out.println("\tSTWA\t" + (location - 2) + ",s");
-
-        }
-
-        if (!(ctx.getParent().getParent().getChild(0).getChild(0).getText().toString().equals("-") || ctx.getParent().getParent().getChild(0).getChild(0).getText().toString().equals("+")) && !funNameCheck && !(ctx.ID().toString().equals("putint")) && !(ctx.ID().toString().equals("getint")) && !(ctx.ID().toString().equals("putchar")) && !(ctx.ID().toString().equals("getchar"))) {
-            //System.out.println(symTab.get(ctx.ID().toString()));
-            System.out.println("\tLDWA\t" + symTab.get(ctx.ID().toString()) + ",s");
-
-        }
-
-
 	}
-
-        @Override
-        public void exitIdTerm(MountCParser.IdTermContext ctx) {
-            String key;
-            int val;
-            //System.out.println("num params is " + numberParameters);
-            //System.out.println(ctx.ID().toString());
-            /*for (int i = numberParameters; i > 0; i -= 1) {
-                key = "key" + i;
-                System.out.println("number of parameters is " + numberParameters);
-                System.out.println(symTab.get(key));
-                val = (symTab.get(key) + (2 * (numberParameters + 1)));
-                symTab.put(ctx.ID().toString(), val);
-            }*/
-            //System.out.println("\tCALL\t" + ctx.ID().toString());
-        }
 
     @Override
     public void enterMathExpr(MountCParser.MathExprContext ctx) {
@@ -513,13 +402,11 @@ public class CompilerListener extends MountCBaseListener {
     @Override
     public void enterTermExpr(MountCParser.TermExprContext ctx) {
 
-
             if (ctx.getParent().getClass().toString().contains("IfExprContext")) {
 
-                String id = ctx.getChild(0).getChild(0).getText().toString();
-                String key3 = "ifcounter" + (howManyIf);
-                int val3 = symTab.get(key3);
-
+                        String id = ctx.getChild(0).getChild(0).getText().toString();
+                        String key3 = "ifcounter" + (howManyIf);
+                        int val3 = symTab.get(key3);
                 if (id.equals("putint") || id.equals("getint") || id.equals("putchar") || id.equals("getchar")) {
                     //System.out.println(ifCounter2);
                     String check = ctx.getParent().getChild(val3).getChild(0).getText().toString();
@@ -544,11 +431,9 @@ public class CompilerListener extends MountCBaseListener {
         //System.out.println(ifCounter2);
 
             if (ctx.getParent().getClass().toString().contains("IfExprContext")) {
-
                 String id = ctx.getChild(0).getChild(0).getText().toString();
                 String key3 = "ifcounter" + (howManyIf);
                 int val3 = symTab.get(key3);
-
                 if (id.equals("putint") || id.equals("getint") || id.equals("putchar") || id.equals("getchar")) {
                     String check = ctx.getParent().getChild(val3).getChild(0).getText().toString();
                     //System.out.println(check);
@@ -566,4 +451,14 @@ public class CompilerListener extends MountCBaseListener {
                 //Do Nothing
             }
     }
+
+    @Override
+    public void exitVarAssign(MountCParser.VarAssignContext ctx) {
+        if (parameters.containsKey(currentID + "_" + ctx.getParent().getChild(0).toString())) {
+            int location = parameters.get(currentID + "_" + ctx.getParent().getChild(0).toString());
+            //System.out.println(currentID + "_" + ctx.getParent().getChild(0).toString() + " " + location);
+            System.out.println("\tSTWA\t" + location + ",s");
+        }
+    }
+
 }
---
+++ Project/12-7/blank-main.c
@@ -0,0 +1,9 @@
+myFun(x, y) {
+    return
+    x = y,
+    putint(x);
+}
+
+main() {
+    return myFun(1,2);
+}
